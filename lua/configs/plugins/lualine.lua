f5aaefb7e916b29d818f089da14cb7e597e0e985
Jiaxu Xing <jiaxu.xing@outlook.com>
2025-03-25 18:17:43 +0100

    Add file name in the lualine

--- lua/configs/plugins/lualine.lua
+++ lua/configs/plugins/lualine.lua
@@ -1,12 +1,12 @@
 return {
 	"nvim-lualine/lualine.nvim",
 	config = function()
-		require('lualine').setup {
+		require("lualine").setup({
 			options = {
 				icons_enabled = true,
-				theme = 'auto',
-				component_separators = { left = '', right = '' },
-				section_separators = { left = '', right = '' },
+				theme = "auto",
+				component_separators = { left = "", right = "" },
+				section_separators = { left = "", right = "" },
 				disabled_filetypes = {
 					"NvimTree",
 					statusline = {},
@@ -19,28 +19,37 @@ return {
 					statusline = 1000,
 					tabline = 1000,
 					winbar = 1000,
-				}
+				},
 			},
 			sections = {
-				lualine_a = { 'filename' },
-				lualine_b = { 'branch', 'diff', 'diagnostics' },
-				lualine_c = {},
+				lualine_a = { "filename" },
+				lualine_b = { "branch", "diff", "diagnostics" },
+				lualine_c = {
+					{
+						"filename",
+						path = 2, -- 1 for relative path, 2 for absolute path
+						file_status = true,
+						newfile_status = true,
+						shorting_target = 40, -- Shorten path if it's longer than this
+						color = { fg = "#7aa2f7", gui = "bold" }, -- Use bright blue color and bold
+					},
+				},
 				lualine_x = {},
-				lualine_y = { 'filesize', 'fileformat', 'filetype' },
-				lualine_z = { 'location' }
+				lualine_y = { "filesize", "fileformat", "filetype" },
+				lualine_z = { "location" },
 			},
 			inactive_sections = {
 				lualine_a = {},
 				lualine_b = {},
-				lualine_c = { 'filename' },
-				lualine_x = { 'location' },
+				lualine_c = { "filename" },
+				lualine_x = { "location" },
 				lualine_y = {},
-				lualine_z = {}
+				lualine_z = {},
 			},
 			tabline = {},
 			winbar = {},
 			inactive_winbar = {},
-			extensions = {}
-		}
-	end
+			extensions = {},
+		})
+	end,
 }
