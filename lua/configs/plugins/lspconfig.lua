ca1f2cca5448728de1a925567adb686b4a164164
Jiaxu Xing <jiaxu.xing@outlook.com>
2025-03-27 13:35:25 +0100

    Remove blink cmp for speed up

--- lua/configs/plugins/lspconfig.lua
+++ lua/configs/plugins/lspconfig.lua
@@ -1,13 +1,12 @@
 -- LSP servers and clients communicate which features they support through "capabilities".
 --  By default, Neovim supports a subset of the LSP specification.
---  With blink.cmp, Neovim has *more* capabilities which are communicated to the LSP servers.
+--  We extend the capabilities with nvim-cmp to support more features.
 --  Explanation from TJ: https://youtu.be/m8C0Cq9Uv9o?t=1275
 --
 -- This can vary by config, but in general for nvim-lspconfig:
 
 return {
 	"neovim/nvim-lspconfig",
-	dependencies = { "saghen/blink.cmp" },
 
 	-- example using `opts` for defining servers
 	opts = {
@@ -17,17 +16,64 @@ return {
 	},
 	config = function(_, opts)
 		local lspconfig = require("lspconfig")
+		local capabilities = vim.lsp.protocol.make_client_capabilities()
+		capabilities = vim.tbl_deep_extend("force", capabilities, {
+			textDocument = {
+				completion = {
+					completionItem = {
+						snippetSupport = true,
+						preselectSupport = true,
+						insertReplaceSupport = true,
+						labelDetailsSupport = true,
+						deprecatedSupport = true,
+						commitCharactersSupport = true,
+						tagSupport = { valueSet = { 1 } },
+						resolveSupport = {
+							properties = {
+								"documentation",
+								"detail",
+								"additionalTextEdits",
+							},
+						},
+					},
+					contextSupport = true,
+					dynamicRegistration = true,
+				},
+			},
+		})
 		for server, config in pairs(opts.servers) do
-			-- passing config.capabilities to blink.cmp merges with the capabilities in your
-			-- `opts[server].capabilities, if you've defined it
-			config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
+			config.capabilities = vim.tbl_deep_extend("force", capabilities, config.capabilities or {})
 			lspconfig[server].setup(config)
 		end
 	end,
 
 	-- example calling setup directly for each LSP
 	config = function()
-		local capabilities = require("blink.cmp").get_lsp_capabilities()
+		local capabilities = vim.lsp.protocol.make_client_capabilities()
+		capabilities = vim.tbl_deep_extend("force", capabilities, {
+			textDocument = {
+				completion = {
+					completionItem = {
+						snippetSupport = true,
+						preselectSupport = true,
+						insertReplaceSupport = true,
+						labelDetailsSupport = true,
+						deprecatedSupport = true,
+						commitCharactersSupport = true,
+						tagSupport = { valueSet = { 1 } },
+						resolveSupport = {
+							properties = {
+								"documentation",
+								"detail",
+								"additionalTextEdits",
+							},
+						},
+					},
+					contextSupport = true,
+					dynamicRegistration = true,
+				},
+			},
+		})
 		local lspconfig = require("lspconfig")
 
 		-- Define on_attach function for key mappings
