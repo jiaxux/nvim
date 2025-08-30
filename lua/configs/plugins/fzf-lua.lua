a964f6ea3c40871358a7c21196599e23a58bd9f6
Jiaxu Xing <jiaxu.xing@outlook.com>
2025-08-30 10:26:00 +0200

    Remove image preview cause we do not need that

--- lua/configs/plugins/fzf-lua.lua
+++ lua/configs/plugins/fzf-lua.lua
@@ -3,6 +3,53 @@ return {
 		"ibhagwan/fzf-lua",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		config = function()
+			local fzf_lua = require("fzf-lua")
+			
+			fzf_lua.setup({
+				previewer = {
+					builtin = {
+						-- Disable snacks integration for previews to avoid conflicts
+						extensions = {
+							["png"] = { "viu", "{file}" },
+							["jpg"] = { "viu", "{file}" },
+							["jpeg"] = { "viu", "{file}" },
+							["gif"] = { "viu", "{file}" },
+							["webp"] = { "viu", "{file}" },
+						},
+						-- Force use of builtin previewer instead of snacks
+						ueberzug_scaler = "cover",
+						title_fnamemodify = function(s) return vim.fn.fnamemodify(s, ":t") end,
+					},
+				},
+				files = {
+					previewer = "builtin",
+					git_icons = false,
+					file_icons = true,
+					color_icons = true,
+					preview_opts = "hidden",
+				},
+				grep = {
+					previewer = "builtin",
+				},
+				buffers = {
+					previewer = "builtin",
+				},
+				-- Ensure fzf-lua handles its own floating window
+				winopts = {
+					preview = {
+						default = "builtin",
+						-- Prevent conflicts with snacks window management
+						border = "rounded",
+						wrap = "nowrap",
+						hidden = "nohidden",
+						vertical = "down:45%",
+						horizontal = "right:50%",
+						layout = "flex",
+						flip_columns = 120,
+					},
+				},
+			})
+			
 			vim.keymap.set("n", "<c-P>", "<cmd>lua require('fzf-lua').files()<CR>")
 			vim.keymap.set("n", "<c-s-B>", "<cmd>lua require('fzf-lua').buffers()<CR>")
 			vim.keymap.set("n", "<c-f>", "<cmd>lua require('fzf-lua').live_grep()<CR>")
